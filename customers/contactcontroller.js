cd137e6843f329ef87113869620fcaf2939bff34
vrushabh4443 <84762906+vrushabh4443@users.noreply.github.com>
2022-10-24 01:09:47 +0530

    Add files via upload

---
+++ customers/contactcontroller.js
@@ -0,0 +1,11 @@
+// const Menu = require('../../models/menu')
+function contactcontroller() {
+    // return {
+    //     async index(req, res) {
+    //         const books = await Menu.find()
+    //         return res.render('home', { books: books })
+    //     }
+    // }
+}
+
+module.exports = contactcontroller
